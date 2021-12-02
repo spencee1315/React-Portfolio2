ccd822e3d9548de373f5f4f5b497433ff0cc3427
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-12-02 12:15:28 -0800

    added App.js structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Social from "./components/Social";
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
-  );
+    <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route component={About()} path='/' exact />
+      <Route component={Portfolio()} path='/portfolio' exact />
+      <Route component={Social()} path='/social' exact />
+    </Switch>
+    <Footer />
+    </BrowserRouter>
+  )
 }
 
 export default App;
