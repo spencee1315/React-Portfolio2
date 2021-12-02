dfd86484a2c93b33dfa71385ffccb72b41a9878b
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-12-02 11:51:23 -0800

    updated portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+import Projects from './Projects';
+
+function ProjectViewer () {
+    return (
+        <Fragment>
+            <Projects />
+        </Fragment>
+    )
+}
+
+export default ProjectViewer;
+
+<section id="projects">
+    <div className="container">
+        <div className="projects">
+            
+        </div>
+    </div>
+</section>
